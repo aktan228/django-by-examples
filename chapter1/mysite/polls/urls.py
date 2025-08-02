8af04f93f26aa1ce956de49075bb16677850e8aa
aktan228 <aktam200e@gmail.com>
2025-08-03 00:45:48 +0600

    just created empty views

--- chapter1/mysite/polls/urls.py
+++ chapter1/mysite/polls/urls.py
@@ -4,6 +4,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("<int:question_id>", views.detail, name="detail"),
+    path("<ing:question_id>", views.results, name="results"),
+    path("<int:question_id>",views.vote,name="vote"),
+    
 ]
--- chapter1/mysite/polls/views.py
+++ chapter1/mysite/polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello, world. you are in a polls index')
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s."  % question_id)
+
+def results(request, question_id):
+    response =  HttpResponse("You're looking at results of question %s.")
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're vooting on question %s." % question_id)
