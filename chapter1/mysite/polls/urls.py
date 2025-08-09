a82b79e72d46c9ec4de94cd8378793841545998c
aktan228 <aktam200e@gmail.com>
2025-08-09 23:58:16 +0600

    fixed 1 problem when page doesnt working

--- chapter1/mysite/polls/urls.py
+++ chapter1/mysite/polls/urls.py
@@ -5,9 +5,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
-    path("<ing:question_id>", views.results, name="results"),
+    path("<int:question_id>", views.results, name="results"),
     path("<int:question_id>/vote/",views.vote,name="vote"),    
 ]
--- chapter1/mysite/polls/views.py
+++ chapter1/mysite/polls/views.py
@@ -10,11 +10,9 @@ from .models import Question, Choice
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date"[:5])
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "pols/index.html", context)
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
