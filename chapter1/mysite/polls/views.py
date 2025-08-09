dc2e39bc9a7660d38f71cb1294ed7fa688654ff7
aktan228 <aktam200e@gmail.com>
2025-08-10 00:37:42 +0600

    i fixed some bugs and stopped on 50page

--- chapter1/mysite/polls/views.py
+++ chapter1/mysite/polls/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.db.models import F
 from django.urls import reverse
-
+from django.conf import settings
+import os
 from .models import Question, Choice
 
 # Create your views here.
@@ -16,13 +17,16 @@ def index(request):
 
 
 def detail(request, question_id):
+    template_path = os.path.join(settings.BASE_DIR, 'polls', 'templates', 'polls', 'detail.html')
+    print(f"Ищем шаблон по пути: {template_path}")
+    print(f"Файл существует: {os.path.exists(template_path)}")
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    response = HttpResponse("You're looking at results of question %s.")
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
@@ -42,4 +46,3 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results",args=(question_id,)))
-    
