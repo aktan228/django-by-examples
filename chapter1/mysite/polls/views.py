67b94a893ed85791315ef2d361ee170fa3ace709
aktan228 <aktam200e@gmail.com>
2025-08-11 00:18:23 +0600

    ive finished 2.6 part

--- chapter1/mysite/polls/views.py
+++ chapter1/mysite/polls/views.py
@@ -29,7 +29,7 @@ class DetailView(generic.DetailView):
     
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'pools/results.html'
+    template_name = 'polls/results.html'
     
 
 def vote(request, question_id):
