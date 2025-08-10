ea6bf23647454bfe9cd0081fc7c3a94aa582229f
aktan228 <aktam200e@gmail.com>
2025-08-10 23:54:30 +0600

    optimized views by using CBV

--- chapter1/mysite/polls/views.py
+++ chapter1/mysite/polls/views.py
@@ -3,31 +3,34 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.db.models import F
 from django.urls import reverse
-from django.conf import settings
-import os
+from django.views import generic
+
 from .models import Question, Choice
 
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     output = ", ".join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    template_path = os.path.join(settings.BASE_DIR, 'polls', 'templates', 'polls', 'detail.html')
-    print(f"Ищем шаблон по пути: {template_path}")
-    print(f"Файл существует: {os.path.exists(template_path)}")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'pools/results.html'
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
